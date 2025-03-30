b3f0880ddcc1c90ab7adfd2cf7980d17d1bab4a5
Davi Gabriel Domingues <domingues.gabriel.davi@gmail.com>
2025-03-30 02:12:30 -0300

    Update Exercício 1.py
    Adição de comentários.

--- DATA/Cursos/2025/Python/Aula 1/Respostas/Exercício 1.py
+++ DATA/Cursos/2025/Python/Aula 1/Respostas/Exercício 1.py
@@ -4,8 +4,8 @@ n = int(input("")) #Valor inicial
 n = 2*n - 1 #Simplificação da sequência: N = 1 + (n-1)*2 dos números naturais ímpares
 
 print()
-for i in range (1, n+1, 2): #adequação proporcional ao valor de n dado no input inicial
+for i in range (1, n+1, 2): #adequação proporcional ao valor de n dado no input inicial (parte superior ao meio do diamante)
     print(" " * ((n-i)//2) + "*" * i)
     
-for i in range (n-2, 0, -2):
+for i in range (n-2, 0, -2): #adequação proporcional ao valor de n dado no input inicial (parte inferior do diamante, após o meio do mesmo)
     print(" " * ((n-i)//2) + "*" * i)
